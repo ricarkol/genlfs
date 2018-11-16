61a06481ef56d218edbf1f43f0200d62b4857c4b
Ricardo Koller <kollerr@us.ibm.com>
2018-11-16 18:58:29 -0500

    better handling of empty files

--- genlfs.c
+++ genlfs.c
@@ -81,10 +81,13 @@ void walk(struct fs *fs, int parent_inum, int inum) {
 		case S_IFREG: {
 			int fd = openat(AT_FDCWD, dirent->d_name, O_RDONLY);
 			assert(fd > 0);
-			void *addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+			void *addr = NULL;
+			if (sb.st_size > 0) {
+				addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+				assert(addr != MAP_FAILED);
+			}
 			int next_inum = get_next_inum();
 			printf("regular file (%d): %s\n", next_inum, dirent->d_name);
-			assert(addr);
 			write_file(fs, (char *)addr, sb.st_size, next_inum,
 					   LFS_IFREG | 0777, 1, 0);
 			munmap(addr, sb.st_size);
